6a77f86a2977a59bf0e1648b092903092d4129b7
Leonid <leo@MacBook-Air-Leonid.local>
2026-02-17 18:09:26 +0500

    Update deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { resolve } from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
