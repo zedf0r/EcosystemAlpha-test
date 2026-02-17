639b6d9e81aafcc464489d144f630a8831763711
zedf0r <164496161+zedf0r@users.noreply.github.com>
2026-02-17 13:12:44 +0000

    Deploying to gh-pages from @ zedf0r/EcosystemAlpha-test@16a362351cff752d50b7c877b213894e99cdf160 🚀

---
+++ assets/Button-DhTYpm45.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-C34ntA_C.js";const u="_button_7sz7d_1",r={button:u},e=({children:t,type:o,onClick:s})=>n.jsx("button",{type:o,className:r.button,onClick:s,children:t});export{e as B};
